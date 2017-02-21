bd81a64b9baa70568a537687bf603f1727ff2226
Timo Goebel <mail@timogoebel.name>
2017-02-21 20:29:08 +0100

    tidy up the code

--- app/controllers/foreman_build_history/build_history_controller.rb
+++ app/controllers/foreman_build_history/build_history_controller.rb
@@ -1,8 +1,5 @@
 module ForemanBuildHistory
   class BuildHistoryController < ApplicationController
-    # change layout if needed
-    # layout 'foreman_build_history/layouts/new_layout'
-
     def index
       @history_facets = BuildHistoryFacet.all
     end
--- lib/foreman_build_history/facet.rb
+++ lib/foreman_build_history/facet.rb
@@ -1,7 +1,1 @@
-Facets.register(BuildHistoryFacet) do
-  # extend_model PuppetHostExtensions
-  # add_helper PuppetFacetHelper
-  # add_tabs :puppet_tabs
-  # api_view :list => 'api/v2/puppet_facets/base', :single => 'api/v2/puppet_facets/single_host_view'
-  # template_compatibility_properties :environment_id, :puppet_proxy_id, :puppet_ca_proxy_id
-end
+Facets.register(BuildHistoryFacet)
